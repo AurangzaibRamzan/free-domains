195a2eb8838c611a717f8195b8b8fbe06f9271b1
aurangzaib <aurangzaib987@gmail.com>
2023-12-11 17:55:25 +0500

    add subdomain aurangzaib

---
+++ domains/aurangzaib-codes.is.an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+    description: 'AURANGZAIB CODES | TEAM LEAD | FULL STACK DEVELOPER',
+    domain: 'is-an.app', 
+    subdomain: 'aurangzaib-codes',
+    owner: {
+      repo: 'https://github.com/AurangzaibRamzan/Personal',
+      email: 'aurangzaib987@gmail.com',
+    },
+    record: {
+      CNAME: 'cname.vercel-dns.com',
+    },
+  });
+  
